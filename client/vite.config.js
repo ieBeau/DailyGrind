7d3a0410629f303b17a8b738e9edf3651ef10609
Beau <77253127+ieBeau@users.noreply.github.com>
2025-10-10 22:08:02 -0400

    feat: Add preview configuration for allowed hosts

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'comp214-dailygrind.onrender.com'
+    ]
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:3000', // proxy to server PORT

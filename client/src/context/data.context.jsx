7e6da1e226f45c15a54165840d11facf604f8963
Beau <77253127+ieBeau@users.noreply.github.com>
2025-12-02 17:35:24 -0500

    refactor: Remove debug log for baskets data in DataProvider

--- client/src/context/data.context.jsx
+++ client/src/context/data.context.jsx
@@ -33,8 +33,6 @@ export const DataProvider = ({ children }) => {
                 })
             );
 
-            console.log('Baskets Data:', basketsData);
-
             handleBaskets(basketsData);
             
             setShoppers(shopperAdditions);
